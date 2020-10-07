2a19d10d580a19b5bd595d1c686b3e0a23e88631
Zafar <zms3342@gmail.com>
2020-10-07 14:45:51 -0400

    set up spotify interaction

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import './components/Login'
+import Player from './components/Player'
 import Login from './components/Login';
+import {getTokenFromResponse} from './spotify.js'
+import SpotifyWebApi from "spotify-web-api-js"
+
+//allows spotify to talk to my react app
+const spotify = new SpotifyWebApi();
 
 function App() {
+
+  const [token, setToken] = useState(null)
+  //getting the token and add state 
+  //will load when once when the app runs, and 
+  //again if the variable changes
+  useEffect(() => {
+    //get the full token
+    const hash = getTokenFromResponse();
+    //in the bar, only display ""
+    window.location.hash = "";
+    //set a _token variable = key
+    const _token = hash.access_token; 
+
+    //update the token state variable
+    if (_token){
+      setToken(_token);
+      spotify.setAccessToken(_token);
+      //async that will help me get the user
+      spotify.getMe().then(user => {
+        console.log("hello", user);
+      } )
+    }
+
+    console.log("i have token", hash);
+
+
+
+  }, []);
+
   return (
     <div className="app">
+    {
+      token ? (
+        <h1>I am logged in</h1>
+      ) : (
+        <Login />
+      )
+    }
       {/* logo */}
       {/* login with spotify button */}
-      <Login />
     </div>
   );
 }
